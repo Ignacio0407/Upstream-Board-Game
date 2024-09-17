ab84b564bc9425570450802fb560d2d836eca1cb
Ignacio0407 <igngutser@alum.us.es>
2024-09-17 19:29:06 +0200

    Poner logo

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../App.css';
 import '../static/css/home/home.css'; 
-import logo from '../static/images/logoUS.png'
+import logo from '../static/images/Upstream_Logo2.jpg'
 
 export default function Home(){
     return(
         <div className="home-page-container">
             <div className="hero-div">
-                <h1>Your game</h1>
+                <h1>Upstream</h1>
                 <h3>---</h3><img src={logo}/>
                 <h3>Do you want to play?</h3>                
             </div>
