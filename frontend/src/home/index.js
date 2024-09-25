ef897d99d6b781fcd771e3ef49577c1ddff27707
Carlos Cerdá Morales <carcermor@alum.us.es>
2024-09-25 18:15:07 +0200

    Commit edición pantalla principal

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import '../App.css';
 import '../static/css/home/home.css'; 
 import logo from '../static/images/Upstream_Logo.jpg'
-
+ 
 export default function Home(){
     return(
         <div className="home-page-container">
             <div className="hero-div">
                 <h1>Upstream</h1>
-                <h3>---</h3><img src={logo}/>
-                <h3>Do you want to play?</h3>                
+                <img src={logo}/>
+                <h1></h1>
+                <h3>¿Jugamos?</h3>                
             </div>
-        </div>
+        </div> // Línea 12 es un espacio en la pantalla
     );
 }
