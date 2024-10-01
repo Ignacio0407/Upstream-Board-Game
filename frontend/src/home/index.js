e0cf41514cca7604b76eda30b817e9cf6da7cd80
Carlos Cerdá Morales <carcermor@alum.us.es>
2024-10-01 11:04:56 +0200

    Fin fondo de inicio Fixed #29

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import '../App.css';
 import '../static/css/home/home.css'; 
-import logo from '../static/images/Upstream_Logo.jpg'
+import logo from '../static/images/logoCentralUPS.jpg'
  
 export default function Home(){
     return(
         <div className="home-page-container">
             <div className="hero-div">
-                <h1>Upstream</h1>
-                <img src={logo}/>
                 <h1></h1>
-                <h3>¿Jugamos?</h3>                
+                <img src={logo}/>
+                <h1></h1>                
             </div>
         </div> // Línea 12 es un espacio en la pantalla
     );
