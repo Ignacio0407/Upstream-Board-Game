3c020b3163ae17777af3b94269741e1d10539775
Ignacio0407 <igngutser@alum.us.es>
2024-10-07 23:33:08 +0200

    Informe de Retrospectiva del Sprint 1

--- frontend/src/dashboard/index.js
+++ frontend/src/dashboard/index.js
@@ -1,33 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { Table, Button } from "reactstrap";
-import useFetchState from "../util/useFetchState"; 
-import tokenService from '../services/token.service'
-import jwt_decode from "jwt-decode";
-import '../static/css/dashboard/dashb.css'
-   
-export default function Dashboard() { 
-    const [username, setUsername] = useState("");
-    const jwt = tokenService.getLocalAccessToken();
-    const [numberOfPlayers, setNumberOfPlayers] = useState(2);
-    const [nombrePartida, setNombrePartida] = useState("");
-    const [crearPartida, setCrearPartida] = useState(2)
+import React, { useEffect } from 'react';
+import '../App.css';
+import '../static/css/dashboard/dashb.css';
+import '../static/css/home/home.css'
+import tokenService from '../services/token.service';
+import BotonLink from '../util/BotonLink';
+
+export default function Home() {
+    const jwt = tokenService.getLocalAccessToken(); 
 
     useEffect(() => {
-        if (jwt) {
-            setUsername(jwt_decode(jwt).sub);
-        }
-    }, [jwt])
+        // Any side effects based on jwt can be handled here
+    }, [jwt]);
 
-    return ( 
-        <> 
-        <div>
-            <div className="prueba"> 
-                <h1>Welcome {username}!</h1>
-                <Button color="success" onClick={() => setNumberOfPlayers(numberOfPlayers+1)}>+</Button>
-                <Button color="danger" onClick={() => setNumberOfPlayers(numberOfPlayers-1)}>-</Button>
-                
+    return (
+        <div className="home-page-container">
+            <div className="pantallaInicio">
+                <div className="game-options">
+                    <div className="game-mode">
+                        <h2>Game Mode</h2>
+                        <button>Normal</button>
+                        <button>Whirlpool</button>
+                        <button>Rapids</button>
+                    </div>
+                    <div className="player-count">
+                        <h2>Number of players</h2>
+                        <button>3 Players</button>
+                        <button>4 Players</button>
+                        <button>5 Players</button>
+                    </div>
+                    <button className="create-game">Create Game</button>
+                </div>
             </div>
-        </div> 
-        </> 
-); 
-} 
+        </div>
+    );
+}
