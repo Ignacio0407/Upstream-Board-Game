0952935a367cf51c86a4f8d6dd26d038628ff915
JoseLu2121 <josmorver@alum.us.es>
2024-10-22 17:33:07 +0200

    Arreglo de fallos para que vaya el backend

--- src/main/java/es/us/dp1/l4_01_24_25/upstream/partidaCasilla/PartidaCasilla.java
+++ src/main/java/es/us/dp1/l4_01_24_25/upstream/partidaCasilla/PartidaCasilla.java
@@ -1,11 +1,15 @@
 package es.us.dp1.l4_01_24_25.upstream.partidaCasilla;
 
+import es.us.dp1.l4_01_24_25.upstream.casilla.Casilla;
 import es.us.dp1.l4_01_24_25.upstream.coordenada.Coordenada;
 import es.us.dp1.l4_01_24_25.upstream.model.BaseEntity;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
-
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name="partidaCasilla")
@@ -13,7 +17,14 @@ public class PartidaCasilla extends BaseEntity {
 
     Integer orientacion;
     Integer capacidad;
+    @Embedded
     Coordenada coordenada;
+    @NotNull
+    @OneToOne(optional=false)
+    @JoinColumn(name="Casilla")
+    Casilla casilla;
+
+
 
 
     
