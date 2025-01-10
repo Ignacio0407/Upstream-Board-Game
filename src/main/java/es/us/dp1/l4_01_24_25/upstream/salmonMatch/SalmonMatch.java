154e5909496298bfc40769271befc63af62c2bac
Ignacio0407 <igngutser@alum.us.es>
2025-01-10 13:15:32 +0100

    Añadidos SalmonMatches con S mayúscula

---
+++ src/main/java/es/us/dp1/l4_01_24_25/upstream/salmonMatch/SalmonMatch.java
@@ -0,0 +1,52 @@
+package es.us.dp1.l4_01_24_25.upstream.salmonMatch;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import es.us.dp1.l4_01_24_25.upstream.coordinate.Coordinate;
+import es.us.dp1.l4_01_24_25.upstream.match.Match;
+import es.us.dp1.l4_01_24_25.upstream.match.PlayerDeserializer;
+import es.us.dp1.l4_01_24_25.upstream.match.PlayerSerializer;
+import es.us.dp1.l4_01_24_25.upstream.model.BaseEntity;
+import es.us.dp1.l4_01_24_25.upstream.player.MatchDeserializer;
+import es.us.dp1.l4_01_24_25.upstream.player.Player;
+import es.us.dp1.l4_01_24_25.upstream.player.matchSerializer;
+import es.us.dp1.l4_01_24_25.upstream.salmon.Salmon;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="salmonMatches")
+public class SalmonMatch extends BaseEntity{
+
+    @ManyToOne
+    @JsonSerialize(using = PlayerSerializer.class)
+	@JsonDeserialize(using = PlayerDeserializer.class)
+    Player player;
+    
+    Integer salmonsNumber;
+    Integer spawningNumber;
+    @Embedded
+    Coordinate coordinate;
+    
+    @NotNull
+    @ManyToOne
+    @JsonSerialize(using = SalmonSerializer.class)
+	@JsonDeserialize(using = SalmonDeserializer.class)
+    Salmon salmon;
+
+    @ManyToOne
+    @JoinColumn(name = "matches", nullable = false)
+    @JsonSerialize(using = matchSerializer.class)
+    @JsonDeserialize(using = MatchDeserializer.class)
+    private Match match;
+
+}
---
+++ src/main/java/es/us/dp1/l4_01_24_25/upstream/salmonMatch/SalmonMatchRepository.java
@@ -0,0 +1,25 @@
+package es.us.dp1.l4_01_24_25.upstream.salmonMatch;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface SalmonMatchRepository extends CrudRepository<SalmonMatch, Integer>{
+    
+    @SuppressWarnings("null")
+    @Override
+    Optional<SalmonMatch> findById(Integer id);
+
+    List<SalmonMatch> findAll();
+    
+    @Query("SELECT sm from SalmonMatch sm WHERE sm.match.id = :matchId")
+    List<SalmonMatch> findAllFromMatch(@Param("matchId") Integer matchId);
+
+    @Query("SELECT sm from SalmonMatch sm WHERE sm.player.id = :playerId")
+    List<SalmonMatch> findAllFromPlayer(@Param("playerId") Integer playerId);
+    @Query("SELECT sm from SalmonMatch sm WHERE sm.match.id = :matchId AND sm.coordinate.y > 20")
+    List<SalmonMatch> findFromGameInSpawn(@Param("matchId") Integer matchId);
+}
